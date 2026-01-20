bc4b0a3633145a3bd99ce6a2e018ab22f06a0a7c
unlie9 <unlie.devops9@gmail.com>
2026-01-20 22:18:13 +0200

    sol

--- task.sh
+++ task.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=ShopDB_full_backup.sql
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --result-file=ShopDB_full_backup.sql
 mysql -u $DB_USER -p$DB_PASSWORD ShopDBReserve < ShopDB_full_backup.sql
 
-mysqldump -u $DB_USER -p$DB_PASSWORD --databases ShopDB --result-file=ShopDB_no_data_backup.sql --no-create-info
+mysqldump -u $DB_USER -p$DB_PASSWORD ShopDB --result-file=ShopDB_no_data_backup.sql --no-create-info
 mysql -u $DB_USER -p$DB_PASSWORD ShopDBDevelopment < ShopDB_no_data_backup.sql
 
 
